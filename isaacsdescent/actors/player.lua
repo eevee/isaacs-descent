392981548685be3c4758ef1f9c9534ec57e992a5
Eevee (Lexy Munroe) <eevee.git@veekun.com>
2016-09-13 18:01:05 -0700

    Make friction work!  Well enough to stand on a slope, anyway

--- isaacsdescent/actors/player.lua
+++ isaacsdescent/actors/player.lua
@@ -51,7 +51,7 @@ local terminal_velocity = 7/8 * PICO8V
 
 function PlayerActor:init(position)
     self.pos = position
-    self.velocity = Vector(0, 0)
+    self.velocity = Vector.zero:clone()
 
     -- TODO arrgh, this global.  sometimes i just need access to the game.
     -- should this be done on enter, maybe?
@@ -66,6 +66,7 @@ function PlayerActor:init(position)
 end
 
 function PlayerActor:update(dt)
+    dt = 1/60
     local xmult
     if self.on_ground then
         xmult = self.ground_friction
@@ -74,8 +75,8 @@ function PlayerActor:update(dt)
     end
     --print()
     --print()
-    --print("position", self.pos)
-    --print(self.velocity, self.min_speed)
+    --print("position", self.pos, "velocity", self.velocity)
+    local vel0 = self.velocity:clone()
 
     -- Explicit movement
     -- TODO should be whichever was pressed last?
@@ -85,7 +86,7 @@ function PlayerActor:update(dt)
     elseif love.keyboard.isDown('left') then
         self.velocity.x = self.velocity.x - self.xaccel * xmult * dt
         self.facing_left = true
-    else
+    elseif self.on_ground then
         -- Decelerate when not holding a key
         -- TODO this still doesn't /quite/ work with slopes because it doesn't
         -- counteract gravity
@@ -96,7 +97,8 @@ function PlayerActor:update(dt)
         end
         if dot ~= 0 then
             decel = decel:projectOn(self.velocity)
-            --self.velocity = self.velocity + decel * dt * xmult
+            --print("decelerating by", decel, decel * dt)
+            self.velocity = self.velocity + decel * dt * xmult
         end
     end
 
@@ -127,23 +129,31 @@ function PlayerActor:update(dt)
     end
 
     -- Gravity
-    local pregrav = self.velocity
     self.velocity = self.velocity + gravity * dt
     self.velocity.y = math.min(self.velocity.y, terminal_velocity)
 
     -- Slope friction
     -- This is different from regular friction, which is about slowing down when not holding a movement key.  Slope friction is your ability to hold yourself still on a slope...
-    if false and self.last_slide then
+    if self.last_slide and self.on_ground then
+        -- TODO only apply if the slope is <= 45
         local slide1 = self.last_slide:normalized()
-        local fric = math.abs(gravity:perpendicular() * slide1)
+        --print("slide:", slide1)
+        local fric = gravity * slide1
         local friction = -fric * slide1
-        --print("velocity before gravity:", pregrav, "magnitude:", pregrav:len())
         --print("velocity before friction:", self.velocity, "magnitude:", self.velocity:len())
         --print("gravity:", gravity)
         --print("friction:", friction, "magnitude:", fric, "/", fric * dt)
-        self.velocity = self.velocity + friction * dt
+        local new_velocity = self.velocity + friction * dt
+        --print("new velocity:", new_velocity)
+        if new_velocity * self.velocity <= 0 then
+            self.velocity = Vector.zero:clone()
+        else
+            self.velocity = new_velocity
+        end
         --print("velocity after friction:", self.velocity)
     end
+
+    --print("total velocity change:", self.velocity - vel0)
 
     -- Velocity-based state tracking
     -- TODO _prevx?
@@ -168,6 +178,7 @@ function PlayerActor:update(dt)
         self.velocity = Vector(-160.89588755785371177,-95.287752296280814335)
         movement = Vector(-3.5,-2.5)
     end
+    -- TODO early return if movement is zero
     --print("Collision time!  position", self.pos, "velocity", self.velocity, "movement", movement)
 
     -- First things first: restrict movement to within the current map
@@ -185,13 +196,13 @@ function PlayerActor:update(dt)
 
     -- Trim velocity as necessary, based on the last surface we slid against
     --print("velocity is", self.velocity, "and clock is", last_clock)
-    if last_clock then
+    if last_clock and self.velocity ~= Vector.zero then
         local axis = last_clock:closest_extreme(self.velocity)
         if not axis then
             -- TODO stop?  once i fix the empty thing
         elseif self.velocity * axis < 0 then
             -- Nearest axis points away from our movement, so we have to stop
-            self.velocity = Vector(0, 0)
+            self.velocity = Vector.zero:clone()
         else
         --print("axis", axis, "dot product", self.velocity * axis)
             -- Nearest axis is within a quarter-turn, so slide that direction
@@ -211,7 +222,9 @@ function PlayerActor:update(dt)
     self.shape:move_to(self.pos:unpack())
 
     -- Update pose depending on movement
+    -- TODO how do these work for things that aren't players?
     local pose
+    -- TODO this is slightly glitchy, has us "walking" on slopes even when still
     if self.velocity.x == 0 then
         pose = 'stand'
     else
