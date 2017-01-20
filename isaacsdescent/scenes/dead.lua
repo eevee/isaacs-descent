74be971d4e5a8c95a5f519c452636738a6ac33e5
Eevee (Lexy Munroe) <eevee.git@veekun.com>
2017-01-19 23:40:57 -0800

    Redo the death overlay...  mostly...  wow this is a mess

--- isaacsdescent/scenes/dead.lua
+++ isaacsdescent/scenes/dead.lua
@@ -47,8 +47,28 @@ function DeadScene:draw()
     -- FIXME this doesn't explain how to use the staff.  i kind of feel like
     -- that should be a ui hint in the background, anyway?  like attached to
     -- the inventory somehow.  maybe you even have to use it
+    local line_height = m5x7:getHeight()
+    local line1 = love.graphics.newText(m5x7, "you died")
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.draw(line1, (w - line1:getWidth()) / 2, h / 2 - line_height + 1)
     love.graphics.setColor(255, 255, 255)
-    love.graphics.printf("you died\npress r to restart", 0, (h - m5x7:getHeight() * 2) / 2, w, "center")
+    love.graphics.draw(line1, (w - line1:getWidth()) / 2, h / 2 - line_height)
+    --local line2 = love.graphics.newText(m5x7, "press R to restart")
+    local line2 = love.graphics.newText(m5x7)
+    line2:set{{255, 255, 255}, "press ", {52, 52, 52}, "R", {255, 255, 255}, " to restart"}
+    local prefixlen = m5x7:getWidth("press ")
+    local keylen = m5x7:getWidth("r")
+    local quad = love.graphics.newQuad(384, 0, 32, 32, p8_spritesheet:getDimensions())
+    love.graphics.setColor(255, 0, 0, 64)
+    --love.graphics.rectangle('fill', (w - line2:getWidth()) / 2, h / 2, line2:getWidth(), line_height)
+    love.graphics.setColor(255, 255, 255)
+    love.graphics.draw(p8_spritesheet, quad, (w - line2:getWidth()) / 2 + prefixlen + keylen / 2 - 32 / 2, h / 2 + line_height / 2 - 32 / 2)
+    --love.graphics.setColor(0, 0, 0)
+    --love.graphics.draw(line2, (w - line2:getWidth()) / 2, h / 2 + 1)
+    love.graphics.setColor(255, 255, 255)
+    love.graphics.draw(line2, (w - line2:getWidth()) / 2, h / 2)
+
+    --love.graphics.printf("you died\npress r to restart", 0, (h - m5x7:getHeight() * 2) / 2, w, "center")
 
     love.graphics.pop()
 end
